248be0ae93e56962e15fa4ea6d9443824d97e7f4
Jessica <ssl3737@hotmail.com>
2022-01-25 19:35:09 +0900

    Added stp_GetStudentById

---
+++ Data/stp/stp_GetStudentById.sql
@@ -0,0 +1,25 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:	Soran Shim
+-- Create date: 2022.01.25
+-- Description:	Get Student data By Id
+-- =============================================
+
+IF OBJECT_ID ( 'stp_GetStudentById', 'P' ) IS NOT NULL   
+    DROP PROCEDURE stp_GetStudentById;  
+GO
+
+CREATE PROCEDURE [dbo].[stp_GetStudentById]
+    @Id int = null
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT StudentId, FullName, Email, Gender
+	FROM Students
+	WHERE StudentId = @Id
+END
+GO
